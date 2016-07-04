d37c9124f785b6196601d639365dc6bef1023697
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-04 13:29:04 -0500

    Initial commit

---
+++ app/src/main/java/g620apps/contactform/ContactFormMainActivity.java
@@ -0,0 +1,17 @@
+package g620apps.contactform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+public class ContactFormMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_form_main);
+
+    }
+}
+
+//need to add the code and hook up the buttons in here.
